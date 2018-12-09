d6313ece00e7f57fdbc99cb43020e6e2bc6041d9
= <timegit13@gmail.com>
2018-12-09 22:32:40 +0300

    Day 8:
    - Move method

--- src/main/java/com/fivan/gameone/entity/mob/Mob.java
+++ src/main/java/com/fivan/gameone/entity/mob/Mob.java
@@ -9,12 +9,26 @@ public abstract class Mob extends Entity {
   protected int dir;
   protected boolean moving;
 
-  public void move() {}
+  public void move(int xa, int ya) {
+    if (xa > 0) { dir = 1; }
+    if (xa < 0) { dir = 3; }
+    if (ya > 0) { dir = 2; }
+    if (ya < 0) { dir = 0; }
 
+    if (!collision()) {
+      x += xa;
+      y += ya;
+    }
+  }
+
+  @Override
   public void update() {}
+
+  public void render() {}
 
   private boolean collision() {
     return false;
   }
+
 
 }
