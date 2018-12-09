df1b515dd136c7423fbc5fdd6904a2982b7857e4
= <timegit13@gmail.com>
2018-12-09 15:04:03 +0300

    Day 8:
    - Basis Player object

---
+++ src/main/java/com/fivan/gameone/entity/mob/Player.java
@@ -0,0 +1,22 @@
+package com.fivan.gameone.entity.mob;
+
+import com.fivan.gameone.graphics.Screen;
+
+public class Player extends Mob {
+
+  public Player() {
+  }
+
+  public Player(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public void update() {
+  }
+
+  @Override
+  public void render(Screen screen) {
+  }
+}
